9554502ac5dc559a240d5cb8f78d0c9a8e3c587c
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-25 21:07:12 +0100

    feedback on input validity

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,36 +1,96 @@
-import classes from './Checkout.module.css';
+import {useRef, useState} from 'react';
+import style from './Checkout.module.css';
+
+const isEmpty = (value) => value.trim().length === 0;
+
+const isFiveLength = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
-  const confirmHandler = (event) => {
-    event.preventDefault();
-  };
+	const [formInputValidity, setFormInputsValidity] = useState({
+		name: true,
+		street: true,
+		city: true,
+		postal: true,
+	});
 
-  return (
-    <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='street'>Street</label>
-        <input type='text' id='street' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
-      </div>
-      <div className={classes.actions}>
-        <button type='button' onClick={props.onCancel}>
-          Cancel
-        </button>
-        <button className={classes.submit}>Confirm</button>
-      </div>
-    </form>
-  );
+	const nameInputRef = useRef();
+	const streetInputRef = useRef();
+	const postalInputRef = useRef();
+	const cityInputRef = useRef();
+
+	const confirmHandler = (event) => {
+		event.preventDefault();
+
+		const enteredName = nameInputRef.current.value;
+		const enteredStreet = streetInputRef.current.value;
+		const enteredPostal = postalInputRef.current.value;
+		const enteredCity = cityInputRef.current.value;
+
+		const enteredNameIsValid = !isEmpty(enteredName);
+		const enteredStreetIsValid = !isEmpty(enteredStreet);
+		const enteredCityIsValid = !isEmpty(enteredCity);
+		const enteredPostalIsValid = isFiveLength(enteredPostal);
+
+		setFormInputsValidity({
+			name: enteredNameIsValid,
+			street: enteredStreetIsValid,
+			city: enteredCityIsValid,
+			postal: enteredPostalIsValid,
+		});
+		const formIsValid =
+			enteredNameIsValid &&
+			enteredStreetIsValid &&
+			enteredCityIsValid &&
+			enteredPostalIsValid;
+
+		if (!formIsValid) {
+			return;
+		}
+		console.log('OK');
+	};
+
+	return (
+		<form className={style.form} onSubmit={confirmHandler}>
+			<div
+				className={`${style.control} ${
+					formInputValidity.name ? '' : style.invalid
+				}`}>
+				<label htmlFor="name">Your Name</label>
+				<input type="text" id="name" ref={nameInputRef} />
+				{!formInputValidity.name && <p> this input is not correct</p>}
+			</div>
+			<div
+				className={`${style.control} ${
+					formInputValidity.street ? '' : style.invalid
+				}`}>
+				<label htmlFor="street">Street</label>
+				<input type="text" id="street" ref={streetInputRef} />
+				{!formInputValidity.street && <p> this input is not correct</p>}
+			</div>
+			<div
+				className={`${style.control} ${
+					formInputValidity.postal ? '' : style.invalid
+				}`}>
+				<label htmlFor="postal">Postal Code</label>
+				<input type="text" id="postal" ref={postalInputRef} />
+				{!formInputValidity.postal && <p> this input is not correct</p>}
+			</div>
+			<div
+				className={`${style.control} ${
+					formInputValidity.city ? '' : style.invalid
+				}`}>
+				<label htmlFor="city">City</label>
+				<input type="text" id="city" ref={cityInputRef} />
+				{!formInputValidity.city && <p> this input is not correct</p>}
+			</div>
+			<div className={style.actions}>
+				<button type="button" onClick={props.onCancel}>
+					Cancel
+				</button>
+				<button className={style.submit}>Confirm</button>
+			</div>
+		</form>
+	);
 };
 
 export default Checkout;
